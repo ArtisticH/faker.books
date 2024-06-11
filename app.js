818ed8f735fa3ced892f71aef3396bc46383d06a
서한나 <hanna@seohannaui-MacBookAir.local>
2024-06-11 14:15:10 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+
+const app = express();
+app.set('port', process.env.PORT || 8081);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/api', (req, res) => {
+  res.sendFile(path.join(__dirname, 'faker.json'));
+})
+app.listen(app.get('port'), () => {
+  console.log(`${app.get('port')}번 포트에서 대기 중`);
+});
